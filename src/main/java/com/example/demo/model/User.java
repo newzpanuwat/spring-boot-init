f9928f9ca6fc2239627b058c949df35791f508bd
New Panuwat <pfcreatorit@gmail.com>
2022-08-17 12:02:32 +0700

    change model to @Data lombok

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,22 +1,12 @@
 package com.example.demo.model;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
 
-@ToString
+@Data
 public class User {
-  @Getter
-  @Setter
   public int userId;
-  @Getter
-  @Setter
   public String username;
-  @Getter
-  @Setter
   public String password;
-  @Getter
-  @Setter
   public String partnerName;
 
   public User(int userId, String username, String password, String partnerName) {
