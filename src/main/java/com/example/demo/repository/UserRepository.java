aee9426b9769669cd95098266fa7070251cb05a6
New Panuwat <new_sbh@me.com>
2022-08-21 20:24:05 +0700

    comment unused

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.User;
+// import com.example.demo.model.User;
 
 // import org.springframework.data.repository.*;
 
-import java.util.List;
+// import java.util.List;
 
 // public interface UserRepository extends CrudRepository<User, Long> {
 // List<User> findByLastName(String partnerName);
